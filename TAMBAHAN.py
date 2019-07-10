777b8181566c149b62e8563b2fa7e5e1a0030ba1
wahyu227 <49665974+wahyu227@users.noreply.github.com>
2019-07-10 16:32:13 +0700

    Add files via upload

---
+++ TAMBAHAN.py
@@ -0,0 +1,26 @@
+import subprocess,os
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+mengetik('GUNAKAN TOOLS DENGAN BIJAK..............')
+mengetik('.')
+mengetik('.')
+mengetik('mencoba meminta akses..')
+mengetik('..KALO ADA MASALAH INSTALL JOIN TELEGRAM................')
+mengetik('.KALO MAU TANYA DM AJA')
+mengetik('.KALO MAU TUTORIAL CEK YOUTUBE FS Tutorial')
+mengetik('......................................')
+mengetik('...mendapatkan file')
+mengetik('...')
+mengetik('...')
+mengetik('mengumpulkan data!')
+mengetik('100%')
+mengetik ('memulai proses')
+mengetik ('menjalankan script')
+
+subprocess.check_output(["am","start","https://www.youtube.com/channel/UC-0ArZPiMhpO4OghqyvEU5w"])
---
+++ aha.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+python2 TAMBAHAN.py
+
+clear
+apt-get update -y
+apt-get upgrade -y
+apt-get install figlet -y
+apt-get install toilet -y
+apt-get install ruby -y
+gem install lolcat -y
+
+clear
+printf "  \e[101m\e[1;77m  :::::::::>>>WELCOME TO<<<:::::::::\e[0m\n"
+printf          "  \e[101m\e[1;77m :::SEMOGA LANCAR INSTALL TOLLS NYA:::\e[0m\n"
+
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet AIRGEDDON . SH | lolcat
+
+sleep 1
+
+echo -e $bu "________________________________________"
+echo -e $bu  " Tools       : AIRGEDDON$white          " 
+echo -e $bu  " Author      : MR.H4NK $white  " 
+echo -e $bu  " Suoport     : FS Tutorial     "
+echo -e $bu  " Telegram    :https://t.me/joinchat/JaUU2BY29eXlZSvfUOU7Rg "
+echo -e $bu  " Contact     : 082279725771 $white " 
+echo -e $bu  "Thank,s for use this tools :)  "
+echo -e $bu  "_______________________________________" 
+
+sleep 1
+apt-get update -y
+apt-get upgrade -y
+apt-get install git -y
+apt-get install php -y
+apt-get install wget -y
+apt-get install python2 -y
+apt-get install python -y
+apt-get install net-tools -y
+apt-get install wireless-tools -y
+apt-get install wireless_tools -y
+apt-get install iw -y
+apt-get install aircrack-ng -y
+apt-get install xterm -y
+apt-get install iproute2 -y
+apt-get install sslstrip -y
+apt-get install asleap -y
+apt-get install bettercap -y
+apt-get install ettercap -y
+apt-get install ettercap-text-only -y
+apt-get install ettercap-graphical -y
+apt-get install expect -y
+apt-get install expect-dev -y
+apt-get install john -y
+apt-get install bully -y
+apt-get install nftables -y
+apt-get install hostapd -y
+apt-get install beef-xss -y
+apt-get install beef-project -y
+apt-get install lighttpd -y
+apt-get install pixiewps -y
+apt-get install reaver -y
+apt-get install isc-dhcp-server -y
+apt-get install dhcp-server -y
+apt-get install dhcp -y
+apt-get install reaver -y
+apt-get install dsniff -y
+apt-get install hostapd-wpe -y
+apt-get install crunch -y
+
+clear
+figlet BENTAR LAGI GAN | lolcat
+
+git clone https://github.com/hashcat/hashcat.git
+cd hashcat
+make
+make install
+cd
+
+git clone https://github.com/aircrack-ng/mdk4
+cd mdk4
+make
+make install
+cd
+
+apt update -y
+apt upgrade -y
+
+git clone https://github.com/v1s1t0r1sh3r3/airgeddon.git
+cd airgeddon
+clear
+bash airgeddon.sh
